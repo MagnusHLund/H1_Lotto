eafd989697253eaf424f1cb46295c9fed6db29f9
Magnus Lund <124877369+MagnusHLund@users.noreply.github.com>
2023-08-14 13:21:12 +0200

    Remove list and add anti number repeat
    Cant repeat the same number and now uses array instead of list.

--- H1_Lotto/Program.cs
+++ H1_Lotto/Program.cs
@@ -1,36 +1,52 @@
-﻿namespace H1_Lotto
+namespace H1_Lotto
 {
     internal class Program
     {
         static void Main()
         {
-            // Creates 3 variables
+            // Creates an array with 8 ints
+            int[] numbers = new int[7];
+
+            // Creates 2 variables
             Random random = new Random();
-            List<int> numbers = new List<int>();
             int joker = 0;
 
-            // Runs a for loop which creates random values and puts them in the list
-            for (int i = 0; i < 8; i++)
+            // Runs a for loop which creates random values and puts them in the array
+            for (int i = 0; i < 7; i++)
             {
-                // Gets random number and places it inside the list
+                // Gets random number
                 int ran = random.Next(1, 37);
-                numbers.Add(ran);
 
                 // This makes sure that there is 1 joker
                 if (i == 0)
                 {
                     joker = random.Next(1, 37);
+
+                    // Adds the joker number to the array
+                    numbers[6] = joker;
                 }
+
+                // If the array already has the number
+                if (numbers.Contains(ran))
+                {
+                    // reduce the for loop int, so the loop runs an additional time, then start loop over
+                    i--;
+                    continue;
+                }
+
+                // Adds the number to the array
+                numbers[i] = ran;
 
             }
 
             // Sorts the numbers in ascending order, except for joker
-            numbers.Sort();
+            Array.Sort(numbers);
 
             // Runs another for loop, which  outputs each value into the console, except for the joker
-            for (int i = 0; i < numbers.Count - 1; i++)
+            for (int i = 0; i < numbers.Length; i++)
             {
                 Console.Write(numbers[i] + " ");
+                Thread.Sleep(2000);
             }
 
             // Changes the text color to red and outputs the joker
